abb80cce9741a4f59b3cfcf355f61251ff19e1f0
t4e1 <jtw5738@naver.com>
2025-08-01 21:34:01 +0900

    basic pr end

---
+++ pr4/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
+func main() {
+	getPages()
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	fmt.Println(res, err)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Html())
+	})
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Reqeust failed with Status: ", res.StatusCode)
+	}
+}
